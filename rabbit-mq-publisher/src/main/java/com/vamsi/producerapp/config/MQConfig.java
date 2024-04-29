265c6e1fff3f52aa11c46898e5c32fa010570775
vamsiborusu28 <vamsiborusu2000@gmail.com>
2024-04-30 00:13:01 +0530

    rabbitmq publisher code is added

---
+++ rabbit-mq-publisher/src/main/java/com/vamsi/producerapp/config/MQConfig.java
@@ -0,0 +1,45 @@
+package com.vamsi.producerapp.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.*;
+
+@Configuration
+public class MQConfig {
+
+    public final static String EMPLOYEEQUEUE = "employee_queue";
+    public final static String employee_route_key = "employee_route_key";
+    public final static String TOPICEXCHANGE = "topic_exchange";
+
+    @Bean
+    public Queue employeeQueue() {
+        return new Queue(EMPLOYEEQUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(TOPICEXCHANGE);
+    }
+
+    @Bean
+    public Binding employeeBinding() {
+        return BindingBuilder.bind(employeeQueue()).to(exchange()).with(employee_route_key);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+}
---
+++ rabbit-mq-publisher/src/main/java/com/vamsi/producerapp/controller/Producer.java
@@ -0,0 +1,28 @@
+
+package com.vamsi.producerapp.controller;
+
+import com.vamsi.producerapp.config.MQConfig;
+import com.vamsi.producerapp.model.Employee;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class Producer {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/employee")
+    public String SaveEmployee(@RequestBody Employee employee) {
+        employee.setEmployeeId(UUID.randomUUID().toString());
+
+        rabbitTemplate.convertAndSend(MQConfig.TOPICEXCHANGE, MQConfig.employee_route_key, employee);
+        return "Employee Saving Success";
+    }
+
+}
